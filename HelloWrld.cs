3b69857a4077cde44feb9a9c1bee1a0ee9e99863
LukySl <156000633+LukySl@users.noreply.github.com>
2024-01-08 17:58:52 +0100

    Add files via upload

---
+++ HelloWrld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hellowrld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Output prn naší knozoli
+
+        print("Hello world!");
+        print(2024);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
